963416d3b85b6a60b4914dccddce42891b3df2cd
Victor Student <vicg@student.chalmers.se>
2022-10-07 08:12:29 +0200

    Initial Network

--- Create_Data.py
+++ Create_Data.py
@@ -1,8 +1,10 @@
 import numpy as np
-from scipy.fft import fft
 import pydub
-import matplotlib.pyplot as plt
+from scipy.signal import spectrogram
+from sklearn.model_selection import train_test_split
 import pandas as pd
+import torch.nn.functional as F
+import torch
 
 
 def read(f, normalized=False):
@@ -18,6 +20,15 @@ def read(f, normalized=False):
 
 metadata = pd.read_csv('archive/xeno-canto_ca-nv_index.csv')
 
+#print(metadata['english_cname'].unique())
+
+y = np.zeros(2730)
+for i in range(91):
+    y[i*30:(i+1)*30] = i
+
+y = torch.from_numpy(y)
+y = F.one_hot(y.to(torch.int64), num_classes=91)
+
 length_sec = metadata['duration_seconds']
 
 max_length = np.max(length_sec * 50000)
@@ -29,4 +40,16 @@ for name in metadata['file_name']:
     if np.ndim(x) > 1:
         x = x[:, 0]
     x = np.concatenate((x, 100 * np.random.normal(size=max_length - len(x))))
-    A.append(plt.specgram(x, Fs=sr))
+    Sxx = spectrogram(x, fs=sr)[2]
+    Sxx_max = np.where(Sxx == np.max(Sxx))
+    Sxx = np.log(Sxx[:, Sxx_max[1][0] - 100:Sxx_max[1][0] + 100])
+    A.append(Sxx)
+
+X_train, X_val, y_train, y_val = train_test_split(A, y,
+                                                  test_size=0.2, random_state=8)
+
+train_data = TensorDataset(X_train, y_train)
+val_data = TensorDataset(X_val, y_val)
+
+train_loader = DataLoader(dataset=train_data, batch_size=25, shuffle=True)
+val_loader = DataLoader(dataset=val_data, batch_size=25)
---
+++ Network.py
@@ -0,0 +1,26 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class BIRDCNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(1, 32, 7, stride=2)
+        self.conv2 = nn.Conv2d(32, 64, 5, stride=2, padding=1)
+        self.pool = nn.MaxPool2d(2)
+        self.conv3 = nn.Conv2d(64, 64, 3, stride=1)
+        self.conv4 = nn.Conv2d(64, 32, 3, stride=1)
+        self.fc1 = nn.Linear(5 * 10 * 32, 128)
+        self.fc2 = nn.Linear(128, 91)
+
+    def forward(self, input_batch):
+        x = self.conv1(input_batch)
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.conv3(x)
+        x = self.pool(F.relu(self.conv4(x)))
+        x = torch.flatten(x, 1)
+        x = F.relu(self.fc1(x))
+        x = F.softmax(self.fc2(x))
+        return x
+
