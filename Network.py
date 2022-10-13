42365a726e0b9a7a80af1fac91c6f72490b0b976
olledahlberg99 <olledahlberg99@hotmail.com>
2022-10-13 15:48:43 +0200

    Update Network.py

--- Network.py
+++ Network.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class StandardCNN(nn.Module):
+class DeepCNN(nn.Module):
     def __init__(self):
         super().__init__()
         self.conv1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=7, stride=2)
@@ -28,7 +28,7 @@ class StandardCNN(nn.Module):
         return x
 
 
-class ShallowCNN(nn.Module):
+class StandardCNN(nn.Module):
     def __init__(self):
         super().__init__()
         self.conv1 = nn.Conv2d(in_channels=1, out_channels=16, kernel_size=7, stride=2)
@@ -49,31 +49,20 @@ class ShallowCNN(nn.Module):
         x = self.fc2(x)
         return x
 
-
-class DeepCNN(nn.Module):
+class ShallowCNN(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=10, stride=2)
-        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=9, stride=2, padding=(0, 1))
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=8, kernel_size=9, stride=2)
+        self.conv2 = nn.Conv2d(in_channels=8, out_channels=16, kernel_size=7, stride=2)
         self.pool = nn.MaxPool2d(2)
-        self.conv3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=7, stride=1)
-        self.conv4 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=5, stride=1, padding=(2, 0))
-        self.conv5 = nn.Conv2d(in_channels=128, out_channels=64, kernel_size=3, stride=1)
-        self.conv6 = nn.Conv2d(in_channels=64, out_channels=32, kernel_size=3, stride=1)
-        self.fc1 = nn.Linear(3 * 8 * 32, 128)
-        self.fc2 = nn.Linear(128, 91)
+        self.fc1 = nn.Linear(12 * 21 * 16, 91)
 
     def forward(self, input_batch):
         x = self.conv1(input_batch)
-        x = self.pool(F.relu(self.conv2(x)))
+        x = self.pool(F.relu(x))
+        x = F.relu(self.conv2(x))
         x = F.normalize(x)
-        x = self.conv3(x)
-        x = F.relu(self.conv4(x))
-        x = F.normalize(x)
-        x = self.conv5(x)
-        x = F.relu(self.conv6(x))
         x = torch.flatten(x, 1)
-        x = F.relu(self.fc1(x))
         x = F.dropout(x, p=0.5)
-        x = self.fc2(x)
+        x = self.fc1(x)
         return x
